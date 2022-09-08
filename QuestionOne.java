41a31ad7bd3d32e4d633478e0e2af18f1be6e566
JustAung <112411778+JustAung@users.noreply.github.com>
2022-09-08 14:36:49 -0400

    Add files via upload

---
+++ QuestionOne.java
@@ -0,0 +1,15 @@
+public class QuestionOne
+{
+
+	public static void main(String args[])
+	{
+		System.out.println("Aung");
+		System.out.println("Pay");
+		System.out.println("Gaming");
+		System.out.println("Computer Science");
+
+	}
+
+
+}
+
---
+++ QuestionTwo.java
@@ -0,0 +1,10 @@
+public class QuestionTwo
+{
+	public static void main(String[] args)
+	{
+		double a = 88.22;
+		int b = 45;
+       		double c = a*b;
+      		System.out.println(a + "*" + b + " = " + c);
+  	 }
+}
