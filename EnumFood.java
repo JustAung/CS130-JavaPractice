e3c55a95906e552974c8cd0a39741ca46654311e
JustAung <112411778+JustAung@users.noreply.github.com>
2022-09-26 11:46:12 -0400

    Add files via upload

---
+++ EnumFood.java
@@ -0,0 +1,18 @@
+enum Food
+{ Durian, Mango, Rice, Noodles}
+public class EnumFood {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Food c1=Food.Durian; Food c2=Food.Mango; Food c3=Food.Rice; Food c4=Food.Noodles;
+		System.out.println(c1);
+		System.out.println(c1.ordinal());
+		System.out.println(c2);
+		System.out.println(c2.ordinal());
+		System.out.println(c3);
+		System.out.println(c3.ordinal());
+		System.out.println(c4);
+		System.out.println(c4.ordinal());
+	}
+
+}
